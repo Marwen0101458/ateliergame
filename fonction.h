a86bae51413315cd6dfc0318f546bb042e3b4d73
Marwen0101458 <Marwen.masmoudi@esprit.tn>
2022-03-09 21:16:52 +0100

    debut .h

---
+++ fonction.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL/SDL.h>
+#include <stdbool.h>
+#include <math.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_ttf.h>
+#include <SDL/SDL_mixer.h>
+typedef struct perso
+{
+
+
+
+
+
+}perso;
