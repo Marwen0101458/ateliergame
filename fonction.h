b797331bf373512446d924e0b3d7766a891f891a
Marwen0101458 <Marwen.masmoudi@esprit.tn>
2022-03-10 09:00:42 +0100

    maaj

--- fonction.h
+++ fonction.h
@@ -14,3 +14,10 @@ typedef struct ennemi
 
 
 }ennemi;
+
+
+void initEnnemi(Ennemi*e)
+void afficherEnnemi(Ennemi e, SDL_Surface * screen)
+void animerEnnemi( Ennemi * e)
+void deplacer( Ennemi * e)
+int collisionBB( personne p, Ennemi e) ou int collisionBB( SDL_Rect posp, SDL_Rect pose)
